3fb30ea407077ddf85d387ff8757716c63415009
Hsbalazs <hoffartscharfi@t-online.hu>
2024-03-21 13:16:23 +0100

    Excercises 10 finished

---
+++ Sud/src/Fox.java
@@ -0,0 +1,12 @@
+public class Fox {
+    public String name;
+    public int age;
+
+    public String color;
+
+    public Fox(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+}
